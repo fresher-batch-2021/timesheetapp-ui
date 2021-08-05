b2e4e1c7e6fe444a28404f5f380730044b926efa
Agathiyan <84656516+be-bees@users.noreply.github.com>
2021-08-05 11:54:10 +0530

    add qr and login function

---
+++ login.js
@@ -0,0 +1,33 @@
+
+      function login() {
+        // event.preventDefault();
+        const email = document.querySelector("#staffEmail").value.toLowerCase();
+        const password = document.querySelector("#staffPassword").value;
+
+        if(email  == null || email == "" || email.trim()  == ""){
+          alert("Name cannot be blank");
+          return false;
+        } else if(password == null || password == "" || password.trim() == ""){
+          alert("Password cannot be blank");
+          return false;
+        }
+          else {
+          console.log ("Your email is "+ email + " and your Password is "+password);
+
+        let loginUserData = {
+          "UserName" : email,
+          "Password" : password,
+
+        }
+        console.log(loginUserData);
+        }
+        
+           if(loginUserData === data.loginUserData){
+            console.log("Success, You're in!")
+          }else {
+            console.log("You're not registered, Kindly contact your manager")
+          }
+        
+      };
+      
+    
