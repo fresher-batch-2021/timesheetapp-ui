ff2f268dfa589a56b0da26eb62c6dff0103e7b0c
Agathiyan V <agathi.nv@gmail.com>
2021-08-09 22:45:05 +0530

    name convention checked

--- timesheet_record.js
+++ timesheet_record.js
@@ -2,10 +2,17 @@ $("#header").load("header.html");
 $("#footer").load("footer.html");
 
 function retrieveTask() {
-  // let tasksStr = localStorage.getItem("TASKS");
-  // let tasks = JSON.parse(tasksStr);
-  // console.log(tasks);
-  document.getElementById("table-to-body").innerHTML = localStorage.getItem("TASKS");
+  let tasksStr = localStorage.getItem("TASKS");
+   let tasks = JSON.parse(tasksStr);
+   console.log(tasks);
+   let content= "";
+   for(let task of tasks){
+     
+    content += "<tr><td>" + task.taskName + "</td><td>" + task.inTime + "</td><td>" + task.outTime + "</td><td>" +
+    task.totalHours + "</td><td>" + task.comments + "</td></tr>";
+   }
+   console.log(content);
+  document.getElementById("table-to-body").innerHTML = content;
 }
 
 
