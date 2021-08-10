dbe1c6620a45e6648d6fed09dd50b0e46e1e8e0f
Agathiyan V <agathi.nv@gmail.com>
2021-08-10 14:28:17 +0530

    register add and validated

--- login.js
+++ login.js
@@ -2,28 +2,17 @@ $("#header").load("header.html");
 $("#footer").load("footer.html");
 
 function login() {
+  
   event.preventDefault();
   const email = document.querySelector("#staffEmail").value.toLowerCase();
   const password = document.querySelector("#staffPassword").value;
 
-  if (email == null || email == "" || email.trim() == "") {
-    alert("Name cannot be blank");
-    return false;
-  } else if (password == null || password == "" || password.trim() == "") {
-    alert("Password cannot be blank");
-    // return false;
-  }
- 
-  else if (email != "agathiyan@gmail.com" || password != "agathi@123") {
-    alert("Email or password is invalid");
-    return false;
-  } else {
 
-
-    alert("success");
-    window.location.href = "timesheet.html";
+  switch(true){
+    case (email == null || email == "" || email.trim() == ""):{alert("Name cannot be blank");break;}
+    case (password == null || password == "" || password.trim() == ""):{alert("Password cannot be blank");break;}
+    case (email != "agathiyan@gmail.com" || password != "agathi@123"):{alert("Email or password is invalid");break;}
+    default:{alert("success");  window.location.href = "timesheet.html";}
   }
 
-
-
-};
+}
