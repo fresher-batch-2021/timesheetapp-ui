38189000a2872f7f27dc8a55b6f5689e457eac88
Agathiyan V <agathi.nv@gmail.com>
2021-08-10 16:08:19 +0530

    login and register api add

--- login.js
+++ login.js
@@ -11,8 +11,27 @@ function login() {
   switch(true){
     case (email == null || email == "" || email.trim() == ""):{alert("Name cannot be blank");break;}
     case (password == null || password == "" || password.trim() == ""):{alert("Password cannot be blank");break;}
-    case (email != "agathiyan@gmail.com" || password != "agathi@123"):{alert("Email or password is invalid");break;}
-    default:{alert("success");  window.location.href = "timesheet.html";}
+    // case (email != "agathiyan@gmail.com" || password != "agathi@123"):{alert("Email or password is invalid");break;}
+    default:{
+      // connecting to server
+      const loginObj={
+        "email":email,
+        "password":password
+      };
+      //login url
+      const url="https://product-mock-api.herokuapp.com/timesheetapp/api/v1/auth/login";
+
+      axios.post(url,loginObj).then(res =>{
+        //for printing 
+        console.log(res);
+        alert("login succesfull");
+        window.location.href="timesheet.html";
+      }).catch(err =>{
+        console.log(err);
+        alert("unable to login");
+
+      });
+    }
   }
 
 }
