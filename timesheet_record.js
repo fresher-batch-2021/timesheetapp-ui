a2b8602b67523b8f0c10503dc2bf71595168d910
Agathiyan V <agathi.nv@gmail.com>
2021-08-11 09:56:03 +0530

    api for timesheet record

--- timesheet_record.js
+++ timesheet_record.js
@@ -2,26 +2,7 @@ $("#header").load("header.html");
 $("#footer").load("footer.html");
 
 function retrieveTask() {
-  let tasksStr = localStorage.getItem("TASKS");
-   let tasks = JSON.parse(tasksStr);
-   console.log(tasks);
-   let content= "";
-   let url = "https://product-mock-api.herokuapp.com/timesheetapp/api/v1/tasks";
-   axios.get(url).then(res => {
-     alert("getting data");
-     let tasks = res.data;
-
-     for(let task of tasks){
-     
-      content += "<tr><td>" + task.taskName + "</td><td>" + task.inTime + "</td><td>" + task.outTime + "</td><td>" +
-      task.totalHours + "</td><td>" + task.comments + "</td></tr>";
-     }
-     console.log(content);
-    document.getElementById("table-to-body").innerHTML = content;
-   }).catch(err => {
-     alert("error in getting data");
-
-   });
+  
    
 }
 
